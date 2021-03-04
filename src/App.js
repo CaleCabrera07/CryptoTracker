78f08df51459883a20e1cb1c97e6687f0d857a08
Carlos Cabrera Semidey <ccarlosalejandros@gmail.com>
2021-03-04 15:13:02 -0500

    CryptoTracker

--- src/App.js
+++ src/App.js
@@ -4,66 +4,66 @@ import './App.css';
 import Coin from './components/Coin';
 
 function App() {
-  const [coins, setCoins] = useState([]);
-  const [search, setSearch] = useState('');
+    const [coins, setCoins] = useState([]);
+    const [search, setSearch] = useState('');
 
-  useEffect(() => {
-    axios
-      .get(
-        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
-      )
-      .then( res => {
-        setCoins(res.data);
-        //console.log(res.data);
-      })
-      .catch(error => console.log(error));
-  }, []);
+    useEffect(() => {
+        axios
+            .get(
+                'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=150&page=1&sparkline=false'
+            )
+            .then((res) => {
+                setCoins(res.data);
+                //console.log(res.data);
+            })
+            .catch((error) => console.log(error));
+    }, []);
 
-  const handleChange = e => {
-    setSearch(e.target.value);
-  };
+    const handleChange = (e) => {
+        setSearch(e.target.value);
+    };
 
-  const filteredCoins = coins.filter(coin =>
-    coin.name.toLowerCase().includes(search.toLowerCase())
-  );
+    const filteredCoins = coins.filter((coin) =>
+        coin.name.toLowerCase().includes(search.toLowerCase())
+    );
 
-  return (
-    <div className='coin-app'>
-      <div className='coin-search'>
-        <h1 className='coin-text'>Search a currency</h1>
-        <form>
-          <input
-            className='coin-input'
-            type='text'
-            onChange={handleChange}
-            placeholder='Bitcoin, Ethereum, Cardano...'
-          />
-        </form>
-      </div>
-      <div className="coin-information">
-        <p>Name</p>
-        <p>Symbol</p>
-        <p>Price</p>
-        <p>Volume</p>
-        <p>Rate</p>
-        <p>Market Cap</p>
-      </div>
-      {filteredCoins.map(coin => {
-        return (
-          <Coin
-            key={coin.id}
-            name={coin.name}
-            price={coin.current_price}
-            symbol={coin.symbol}
-            marketcap={coin.total_volume}
-            volume={coin.market_cap}
-            image={coin.image}
-            priceChange={coin.price_change_percentage_24h}
-          />
-        );
-      })}
-    </div>
-  );
+    return (
+        <div className='coin-app'>
+            <div className='coin-search'>
+                <h1 className='coin-text'>Search a currency</h1>
+                <form>
+                    <input
+                        className='coin-input'
+                        type='text'
+                        onChange={handleChange}
+                        placeholder='Bitcoin, Ethereum, Cardano...'
+                    />
+                </form>
+            </div>
+            <div className='coin-information'>
+                <p>Name</p>
+                <p>Symbol</p>
+                <p>Price</p>
+                <p>Volume</p>
+                <p>Rate</p>
+                <p>Market Cap</p>
+            </div>
+            {filteredCoins.map((coin) => {
+                return (
+                    <Coin
+                        key={coin.id}
+                        name={coin.name}
+                        price={coin.current_price}
+                        symbol={coin.symbol}
+                        marketcap={coin.total_volume}
+                        volume={coin.market_cap}
+                        image={coin.image}
+                        priceChange={coin.price_change_percentage_24h}
+                    />
+                );
+            })}
+        </div>
+    );
 }
 
 export default App;
